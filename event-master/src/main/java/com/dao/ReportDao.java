11a476f2c74093d439ae61f89d080486827e0c5d
hanunhang <116699063+hanunhang@users.noreply.github.com>
2023-06-21 21:19:48 +0800

    Initial commit

---
+++ event-master/src/main/java/com/dao/ReportDao.java
@@ -0,0 +1,24 @@
+package com.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+import com.entity.ReportEntity;
+import com.entity.view.ReportView;
+import com.entity.vo.ReportVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ReportDao extends BaseMapper<ReportEntity> {
+
+    List<ReportVO> selectListVO(@Param("ew") Wrapper<ReportEntity> wrapper);
+
+    ReportVO selectVO(@Param("ew") Wrapper<ReportEntity> wrapper);
+
+    List<ReportView> selectListView(@Param("ew") Wrapper<ReportEntity> wrapper);
+
+    List<ReportView> selectListView(Pagination page, @Param("ew") Wrapper<ReportEntity> wrapper);
+
+    ReportView selectView(@Param("ew") Wrapper<ReportEntity> wrapper);
+}
